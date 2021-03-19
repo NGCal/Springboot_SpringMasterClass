3a440cac3795ea754b253021ef8c577fa640ec97
nathgcal <nathalia.gcalzado@hotmail.com>
2021-03-18 22:06:48 -0400

    Example of the Around Aspect

--- src/main/java/com/springbasics/web/springboot/Aspects/AspectClass.java
+++ src/main/java/com/springbasics/web/springboot/Aspects/AspectClass.java
@@ -1,6 +1,7 @@
 package com.springbasics.web.springboot.Aspects;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +25,14 @@ public class AspectClass {
     @AfterThrowing(value = "execution(* com.springbasics.web.springboot.Bussines.*.*(..))", throwing = "exception")
     private void checkForException(JoinPoint joinPoint, Exception exception) {
         LOG.info("Method {} throw exception {}", joinPoint, exception);
+    }
+
+    @Around(value = "execution(* com.springbasics.web.springboot.Bussines.*.*(..))")
+    private void trackTime(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        proceedingJoinPoint.proceed();
+        long executionTime = System.currentTimeMillis() - startTime;
+        LOG.info("Method {} ran {} milliseconds", proceedingJoinPoint, executionTime);
     }
 
 
