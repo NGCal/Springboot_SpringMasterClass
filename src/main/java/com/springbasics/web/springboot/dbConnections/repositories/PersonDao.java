9c2450291781d6e964d83e6d3722fb1f720501a3
nathgcal <nathalia.gcalzado@hotmail.com>
2021-03-20 14:35:11 -0400

    Created a custom RowMapper for Person

--- src/main/java/com/springbasics/web/springboot/dbConnections/repositories/PersonDao.java
+++ src/main/java/com/springbasics/web/springboot/dbConnections/repositories/PersonDao.java
@@ -5,8 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
@@ -18,7 +21,7 @@ public class PersonDao {
 
     public List<Person> retrieveAll() {
         List<Person> people = jdbcTemplate.query("select * from PERSON",
-                new BeanPropertyRowMapper(Person.class));
+                new PersonRowMapper());
         return people;
     }
 
@@ -33,6 +36,21 @@ public class PersonDao {
                 "BIRTH_DATE = ? where id = ?", new Object[]{person.getName(), person.getLocation(),
                 new Timestamp(person.getBirthDate().getTime()), person.getId()});
         return matchingPerson > 0;
+    }
+
+    class PersonRowMapper implements RowMapper<Person> {
+
+        @Override
+        public Person mapRow(ResultSet resultSet, int i) throws SQLException {
+
+            Person person = new Person();
+
+            person.setId(resultSet.getInt("id"));
+            person.setName(resultSet.getString("name"));
+            person.setLocation(resultSet.getString("location"));
+            person.setBirthDate(resultSet.getTimestamp("birth_date"));
+            return person;
+        }
     }
 
 
